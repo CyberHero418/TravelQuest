f81b42ac7482b4f04bbbbdffffb12cead2c6509b
Kazi Fahim Rashid <cyberhero418@gmail.com>
2024-09-26 10:30:26 +0600

    4

--- config.php
+++ config.php
@@ -1,22 +1,17 @@
 <?php
-define('DB_HOST', 'localhost');    // Database host (e.g., localhost)
-define('DB_USER', 'root'); // Database username
-define('DB_PASS', ''); // Database password
-define('DB_NAME', 'travelquest'); // Database name
+define('DB_HOST', 'localhost');    
+define('DB_USER', 'root'); 
+define('DB_PASS', ''); 
+define('DB_NAME', 'travelquest'); 
 
 
 
-// Establish a connection to the database using MySQLi or PDO
+
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
-// Check the connection
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-?>
-<?php
-
-echo "Hello word !!!!!!";
-
 ?>
 
